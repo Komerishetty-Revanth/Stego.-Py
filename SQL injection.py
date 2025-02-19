09d73c7e0aa04bab2fc5c9668badafd6c0674143
Komerishetty-Revanth <revanthgputha@gmail.com>
2025-02-19 21:46:29 +0530

    Update SQL injection.py

--- SQL injection.py
+++ SQL injection.py
@@ -4,10 +4,10 @@
  import connect_db
  import send_alert
 
-def detect_sql_injection(user_input):
-    if validate_input(user_input):
-        parsed_sql = parse_sql(user_input)
-        if parsed_sql['is_injection'):
+def detect_sql_injection(user_input);
+    if validate_input(user_input);
+        parsed_sql = parse_sql(user_input);
+        if parsed_sql['is_injection');
             send_alert(parsed_sql)
             return "SQL Injection Detected"
         else:
